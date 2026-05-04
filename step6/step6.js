27587718c07c292c23b12aa43cd14e56aa89d531
Samantha1919 <samantha.schif@gmail.com>
2026-05-04 16:01:58 +0200

    feat: add debut step6 and other qw

---
+++ step6/step6.js
@@ -0,0 +1,40 @@
+let params = new URLSearchParams(document.location.search);
+let h1 = document.getElementById("h1");
+let h2 = document.getElementById("h2");
+let color = params.get("color") ?? "yellow"; // le ?? sapelle "nullish coalescing operator" et il sert a donner une valeur par defaut
+let objectColor = {
+  // "un index" "recupere dcp genre yellow avec tout ce quil y a dans les accolades, recup la valeur de yellow"
+  yellow: {
+    backgroundColor: "#F7F4C8",
+    h1: "#EB6E00",
+    h2: "#EB6E00",
+  },
+  pink: {
+    backgroundColor: "#eb8ed8",
+    h1: "#7F1E65",
+    h2: "#7F1E65",
+  },
+  blue: {
+    backgroundColor: "#8ebfeb",
+    h1: "#236CF4",
+    h2: "#236CF4",
+  },
+  random: {
+    backgroundColor: addRandomColor(),
+    h1: addRandomColor(),
+    h2: addRandomColor(),
+  },
+};
+
+function addRandomColor() {
+  // recuper r g  b aleatoirement entre 0 et 255
+  let R = Math.floor(Math.random() * 255); // math random il genre un chiffre entre 0 et 1 dcp tu pexu faire 0,5 x 255
+  let G = Math.floor(Math.random() * 255);
+  let B = Math.floor(Math.random() * 255);
+
+  R = R.toString(16);
+  G = G.toString(16);
+  B = B.toString(16); // retourne 16 psq c en héxadécimal mais dcp pk on a besoin de les changer en string
+
+  return "#" + R + G + B; // prend le hasthag ren html
+}
